9b58869ad136014289502e07517ce402e70f478f
Lucas Bernadino <luucasdinoo@gmail.com>
2024-11-10 16:34:37 -0300

    Initial Setup

---
+++ src/main/java/br/com/terracotabackend/TerracotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.terracotabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerracotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerracotaApplication.class, args);
+    }
+
+}
